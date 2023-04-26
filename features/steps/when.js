f89c7732cdb6bda3fd778c73d170afdc7bff2b59
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2023-04-26 17:42:17 +0900

    some fix

--- features/steps/when.js
+++
@@ -1,6 +0,0 @@
-const { When } = require('@cucumber/cucumber')
-const { addLocations, textToLngLat } = require('../support/module')
-
-When(/^現在位置は(.+?)である。$/, function(text) {
-  addLocations(this, textToLngLat(text))
-});
