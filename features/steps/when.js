bc98b6d0bcd59e91cffe8189ca78da8a76938a6b
naoki <n.globe.us@gmail.com>
2023-04-26 22:31:29 +0900

    some fix

--- features/steps/when.js
+++
@@ -1,6 +0,0 @@
-const { When } = require('@cucumber/cucumber')
-const { addLocations, textToLngLat } = require('../support/module')
-
-When(/^現在位置は(.+?)である。$/, async function(text) {
-  addLocations(this, await textToLngLat(text))
-});
