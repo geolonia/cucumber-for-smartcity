0e16784d648af8f5bccf97b5183e289d36651056
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2023-04-28 01:34:00 +0900

    some fix

--- features/steps/then.mjs
+++ features/steps/then.mjs
@@ -14,36 +14,48 @@ Then(/そこには建築物がある。/, function() {
   let result = false
 
   const current = getLocations(this).slice(-1)[0]
-  const tile = lnglatToTile(current[0], current[1], 16)
 
-  return getTile('https://cyberjapandata.gsi.go.jp/xyz/optimal_bvmap-v1/{z}/{x}/{y}.pbf', ...tile).then(features => {
-    for (let i = 0; i < features.length; i++) {
-      if (geoContains(features[i], current)) {
-        if (features[i].features[0].properties.vt_code && 3100 <= features[i].features[0].properties.vt_code && 4000 >= features[i].features[0].properties.vt_code) {
-          result = true
+  if (current.length) {
+    const tile = lnglatToTile(current[0], current[1], 16)
+
+    return getTile('https://cyberjapandata.gsi.go.jp/xyz/optimal_bvmap-v1/{z}/{x}/{y}.pbf', ...tile).then(features => {
+      for (let i = 0; i < features.length; i++) {
+        if (geoContains(features[i], current)) {
+          if (features[i].features[0].properties.vt_code && 3100 <= features[i].features[0].properties.vt_code && 4000 >= features[i].features[0].properties.vt_code) {
+            result = true
+          }
         }
       }
-    }
 
-    assert.equal(true, result)
-  })
+      if (false === result) {
+        throw new Error('建築物が見つかりませんでした。')
+      }
+    })
+  } else {
+    throw new Error('建築物が見つかりませんでした。')
+  }
 })
 
 Then(/そこには建築物がない。/, function() {
   let result = false
 
   const current = getLocations(this).slice(-1)[0]
-  const tile = lnglatToTile(current[0], current[1], 16)
 
-  return getTile('https://cyberjapandata.gsi.go.jp/xyz/optimal_bvmap-v1/{z}/{x}/{y}.pbf', ...tile).then(features => {
-    for (let i = 0; i < features.length; i++) {
-      if (geoContains(features[i], current)) {
-        if (features[i].features[0].properties.vt_code && 3100 <= features[i].features[0].properties.vt_code && 4000 >= features[i].features[0].properties.vt_code) {
-          result = true
+  if (current.length) {
+    const tile = lnglatToTile(current[0], current[1], 16)
+
+    return getTile('https://cyberjapandata.gsi.go.jp/xyz/optimal_bvmap-v1/{z}/{x}/{y}.pbf', ...tile).then(features => {
+      for (let i = 0; i < features.length; i++) {
+        if (geoContains(features[i], current)) {
+          if (features[i].features[0].properties.vt_code && 3100 <= features[i].features[0].properties.vt_code && 4000 >= features[i].features[0].properties.vt_code) {
+            result = true
+          }
         }
       }
-    }
 
-    assert.equal(false, result)
-  })
+      if (true === result) {
+        throw new Error('建築物があります。')
+      }
+    })
+  }
 })
